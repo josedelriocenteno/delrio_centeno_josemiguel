0dacb8d6e55ecbe867024eb9e9adf52ac977ec5d
josedelriocenteno <josemiguel.delrio.centeno@gregoriofer.com>
2026-02-09 14:41:00 +0100

    1

---
+++ src/main/java/com/delcenjo/delrio_centeno_josemiguel/Delrio_centeno_josemiguel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.delcenjo.delrio_centeno_josemiguel;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class Delrio_centeno_josemiguel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
